17dbfe757a0b6dcf859843c8667d04968ac4b5c6
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-03-07 07:54:44 +0530

    Used yield, getname, setname, isalive, currenthread
    Used methods currentThread(), isAlive() , setName(String name) , getName() , yield().

--- multithreading/Thread Class Methods/NewThread.java
+++ multithreading/Thread Class Methods/NewThread.java
@@ -1,14 +1,24 @@
-package multithreading.runnableinterface;
+package multithreading.threadclass;
 
-public class NewThread implements Runnable {
-
+public class NewThread extends Thread{
+	
+	// This is the new thread class
+	
+	
 	@Override
 	public void run() {
-		for(int j = 1; j <= 50 ; j++) {
-			
-			System.out.println("Hellow Dnyanesh..");
+		
+		// Here we write the thread logic 
+		
+		// now we have to use the join method of the thread class
+		// it stops the execution of one thread and runs another thread with similar priority
+		
+		for(int h = 1; h <= 100; h++) {
+			System.out.println("Thread : " + h);
 		}
 		
+		
 	}
+	
 
 }
